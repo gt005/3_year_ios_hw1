de30eeb161cb6dac9c3c5d2f9295a27d302a62a6
Karim <karimhamid@yandex.ru>
2024-02-04 20:12:35 +0300

    Initial Commit

---
+++ 3_year_ios_hw1/__year_ios_hw1App.swift
@@ -0,0 +1,17 @@
+//
+//  __year_ios_hw1App.swift
+//  3_year_ios_hw1
+//
+//  Created by Карим Хамид on 04.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct __year_ios_hw1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
